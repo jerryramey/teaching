3be4bbfae11032a2a28bfd9d535f09423d03b89d
Jerry  Ramey <jerryramey@Mac-Studio.local>
2023-10-24 10:28:49 -0600

    Student initialize with first and last name completed

--- student.rb
+++ student.rb
@@ -4,3 +4,14 @@
 # Set their first and last name
 # Get their name
 
+class Student
+
+  @first_name = ""
+  @last_name = ""
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+end
